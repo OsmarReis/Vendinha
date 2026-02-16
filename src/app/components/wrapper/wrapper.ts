3f9d0d485f50bee8a3ce612152fa24180adbc51f
Osmar Reis <osmarzero26@gmail.com>
2026-02-16 20:46:18 -0300

    Added cli annonymous analytics. Changed some css classes of wrapper and nav-menu components.

--- src/app/components/wrapper/wrapper.ts
+++ src/app/components/wrapper/wrapper.ts
@@ -4,7 +4,7 @@ import { Component } from "@angular/core";
     selector: "app-wrapper",
     imports: [],
     template: `
-        <div class="w-full p-2.5 flex flex-col rounded-lg bg-white drop-shadow">
+        <div class="w-full p-2.5 flex flex-col gap-2.5 rounded-lg bg-white drop-shadow">
             <ng-content />
         </div>
     `
